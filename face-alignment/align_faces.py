d9f1a7e87f76a61b39542f90b740a1cadb714e9b
Davi-Schumacher <daschumacher@dons.usfca.edu>
2019-01-24 20:49:30 -0800

    initial commit of face alignment

---
+++ face-alignment/align_faces.py
@@ -0,0 +1,55 @@
+# USAGE
+#python align_faces.py --shape-predictor shape_predictor_68_face_landmarks.dat --image images/example_01.jpg
+# for file in images/*.jpg do
+# python align_faces.py --shape-predictor shape_predictor_68_face_landmarks.dat --image $file
+# end
+# import the necessary packages
+from imutils.face_utils import FaceAligner
+from imutils.face_utils import rect_to_bb
+import argparse
+import imutils
+import dlib
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+args = vars(ap.parse_args())
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor and the face aligner
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+fa = FaceAligner(predictor, desiredFaceWidth=256)
+
+# load the input image, resize it, and convert it to grayscale
+image = cv2.imread(args["image"])
+s_name = ''.join(args["image"].split('/')[-1].split('-')[-2:])[:-4]
+image = imutils.resize(image, width=800)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# show the original input image and detect faces in the grayscale
+# image
+#cv2.imshow("Input", image)
+rects = detector(gray, 2)
+
+# loop over the face detections
+for rect in rects:
+	# extract the ROI of the *original* face, then align the face
+	# using facial landmarks
+	(x, y, w, h) = rect_to_bb(rect)
+	faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
+	faceAligned = fa.align(image, gray, rect)
+
+	import uuid
+	f = str(uuid.uuid4())
+	cv2.imwrite("foo/" + f + ".png", faceAligned)
+
+	# display the output images
+	cv2.imwrite("/Users/davischum/Downloads/aligned/" + s_name + ".png", faceAligned)
+	#cv2.imshow("Original", faceOrig)
+	#cv2.imshow("Aligned", faceAligned)
+	#cv2.waitKey(0)
